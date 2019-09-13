3d334e93457f7b59435058e882826fda5a20fa3f
Felix Sun <sunfmin@gmail.com>
2019-09-13 10:05:58 +0800

    add home

--- mux.go
+++ mux.go
@@ -8,11 +8,19 @@ import (
 func Mux() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/", home)
 	return mux
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprint(w, "Hello")
+	_, err := fmt.Fprint(w, "Hello\n")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprint(w, "Home\n")
 	if err != nil {
 		panic(err)
 	}
